b0dd336edc616ebe5650b3d9bada0ae252d7fca7
KennyBeverley <kenneth.beverley@gmail.com>
2022-03-29 14:54:07 -0500

    Create Controller2D.cs

---
+++ Controller2D.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum GameType { SideScroller, Topdown}
+public class Controller2D : MonoBehaviour
+{
+    [SerializeField] private GameType type;
+    [SerializeField]private bool hasGravity;
+    [SerializeField]private float gravityValue;
+    [SerializeField] private bool canJump;
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float horizontalSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (type)
+        {
+            case GameType.SideScroller:
+                break;
+            case GameType.Topdown:
+                hasGravity = false;
+                canJump = false;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
+        {
+            Debug.Log(horizontal);
+            transform.position += new Vector3(horizontal * horizontalSpeed * Time.deltaTime, vertical * verticalSpeed * Time.deltaTime,0);
+        }
+    }
+}
