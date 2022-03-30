27078467ba422d759c1dfa2e5a7b7bb034cc33b5
KennyBeverley <kenneth.beverley@gmail.com>
2022-03-29 20:25:17 -0500

    cursor manager
    updated cursor manager to allow objects to flip and rotate based on cursor position

--- Scripts/CursorManager.cs
+++ Scripts/CursorManager.cs
@@ -6,6 +6,8 @@ public class CursorManager : MonoBehaviour
 {
     [SerializeField]private GameObject currentCursor;
     [SerializeField] private List<GameObject> cursors;
+    public List<CursorObjects> objectsToFaceCursor;
+
     private Vector2 cursorPos;
     private GameObject instantiatedCursor;
 
@@ -21,12 +23,14 @@ public class CursorManager : MonoBehaviour
 
     private void Update()
     {
-        if(currentCursor != null)
-        {
-            
-            cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (currentCursor != null)
+        {                        
             instantiatedCursor.transform.position = cursorPos;
-        }       
+        }
+
+        if(objectsToFaceCursor.Count > 0)        
+            FaceCursor();
     }
 
     public void SetCursor(int index)
@@ -35,6 +39,78 @@ public class CursorManager : MonoBehaviour
             currentCursor = cursors[index];
     }
 
+    private void FaceCursor()
+    {
+        foreach(var val in objectsToFaceCursor)
+        {
+            if(val.gameObject != null)
+            {
+                switch (val.howToFlip)
+                {
+                    case WaysToFlip.none:
+                        break;
+                    case WaysToFlip.xOnly:
+                        FlipOnAxis(Axis.x, val.gameObject);
+                        break;
+                    case WaysToFlip.yOnly:
+                        FlipOnAxis(Axis.y, val.gameObject);
+                        break;
+                }
+
+                if (val.rotateTowardsCursor)
+                    FaceTarget(val.gameObject);
+                
+            }
+            
+        }
+    }
+    private void FaceTarget(GameObject gameObject)
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void FlipOnAxis(Axis axis, GameObject objectToRotate)
+    {
+        switch (axis)
+        {
+            case Axis.x:
+                if (cursorPos.y > objectToRotate.transform.position.y)
+                {
+                    objectToRotate.transform.eulerAngles = new Vector3(180, objectToRotate.transform.eulerAngles.y, 0);
+                }
+                else
+                {
+                    objectToRotate.transform.eulerAngles = new Vector3(0, objectToRotate.transform.eulerAngles.y, 0);
+                }
+
+                break;
+            case Axis.y:
+                if (cursorPos.x > objectToRotate.transform.position.x)
+                {
+                    objectToRotate.transform.eulerAngles = new Vector3(objectToRotate.transform.eulerAngles.x, 0, 0);
+                }
+                else
+                {
+                    objectToRotate.transform.eulerAngles = new Vector3(objectToRotate.transform.eulerAngles.x, 180, 0);
+                }
+                break;
+        }
+    }
 
 
+}
+
+public enum Axis { x, y}
+public enum WaysToFlip { none, xOnly, yOnly}
+
+[System.Serializable]
+public class CursorObjects
+{
+    public GameObject gameObject;
+    public WaysToFlip howToFlip;
+    public bool rotateTowardsCursor;
+    //public bool facesCursorOnX;
+    //public bool facesCursorOnY;
 }
