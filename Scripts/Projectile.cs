78d02000b504ddcdd2478f574b4573409c866bab
KennyBeverley <kenneth.beverley@gmail.com>
2022-03-29 21:40:40 -0500

    updated shooting scripts

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed;
+    Vector3 dir;
+    Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dir = Input.mousePosition - Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        velocity = Vector3.Normalize(dir - transform.position) * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        //transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        transform.position += velocity * Time.deltaTime;
+        //transform.position += dir * speed * Time.deltaTime;
     }
 }
--- Scripts/ShootProjectile.cs
+++ Scripts/ShootProjectile.cs
@@ -7,7 +7,7 @@ public class ShootProjectile : MonoBehaviour
     [SerializeField] private GameObject gun;
     [SerializeField] private GameObject projectile;
     [SerializeField] private GameObject firePoint;
-    private bool targetCursor;    
+    [SerializeField]private bool targetCursor;    
     private Vector2 target;
 
     // Start is called before the first frame update
@@ -19,11 +19,12 @@ public class ShootProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire"))
+        if (Input.GetButtonDown("Fire1"))
         {
             if (targetCursor)
             {
                 var clone = Instantiate(projectile, firePoint.transform.position, gun.transform.rotation);
+                Destroy(clone, 2);
             }
             else
             {
